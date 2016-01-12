4cc8f727e6212f343970ad483706dd21c388d553
Shamima Nasrin <tumpa.fci@gmail.com>
2016-01-13 02:12:43 +0600

    Nothing
    Just copy a project

---
+++ TabFragmentViewPager/app/src/main/java/com/example/tumpa/tabfragmentviewpager/TabFragment2.java
@@ -0,0 +1,19 @@
+package com.example.tumpa.tabfragmentviewpager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tumpa on 12/22/2015.
+ */
+public class TabFragment2 extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+}
